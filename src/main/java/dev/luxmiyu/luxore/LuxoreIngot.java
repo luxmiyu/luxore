8b6fdb3687b19cf0bf63673620ebc2cd430e2afb
LordDeatHunter <desuhansa@gmail.com>
2022-06-09 23:10:40 +0200

    Update to 1.19

--- src/main/java/dev/luxmiyu/luxore/LuxoreIngot.java
+++ src/main/java/dev/luxmiyu/luxore/LuxoreIngot.java
@@ -5,7 +5,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -19,7 +19,7 @@ public class LuxoreIngot extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
         if (!world.isClient) {
-            playerEntity.sendMessage(new LiteralText(playerEntity.getEntityName() + ", you are magical..."), false);
+            playerEntity.sendMessage(Text.translatable("luxore.ingot.on_use", playerEntity.getEntityName()), false);
         }
         return TypedActionResult.success(playerEntity.getStackInHand(hand));
     }
